5e94574d1d1b3749f112b842b5842a2118b2ffcf
jyr <jean-yves.rouseau@orange.fr>
2022-08-23 16:57:49 +0200

    fin de 1 jour

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="hello" v-if="step == 1">
+    <h1>Bienvenu dans le jeu de morpion</h1>
+
+    <div>
+      <p>regle du jeux</p>
+    </div>
+    <input name="joueur 1" type="text" v-model="joueur1" placeholder="joueur 1" />
+    <input name="joueur 2" type="text" v-model="joueur2" placeholder="joueur 2" />
+    <div></div>
+    <button v-on:click="plus">GO</button>
+
+  </div>
+
+  <div class="jeu" v-if="step == 2">
+    <h1>a vous de jouer</h1>
+    <p>{{ joueur1 }}</p>
+    <p>{{ joueur2 }}</p>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      step: 1,
+      joueur1: "joueur1",
+      joueur2: "joueur2",
+    }
+  },
+
+  methods: {
+    plus() {
+      this.step = this.step + 1;
+    }
+
   }
 }
+
+
 </script>
 
 <style>
